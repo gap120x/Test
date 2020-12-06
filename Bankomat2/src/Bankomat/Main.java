cfc5d566e257888c3b17545fac2d7c609ee2eb34
Komp <Komp@192.168.0.25>
2020-12-06 18:57:34 +0100

    nowy commit

---
+++ Bankomat2/src/Bankomat/Main.java
@@ -0,0 +1,12 @@
+package Bankomat;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("giciki");
+System.out.println("test");
+
+	}
+
+}
